6ae0cf5a5f37b5c6d1db6fd17a884f3545e88364
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-22 12:18:08 +0900

    表示確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Title from './components/Title'
+import './App.css'
 
+// 自分で作成したタグの最初の文字は大文字で始めること。＝ReactコンポーネントであることをReactに伝える。
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test">
+      <h1>Hello!</h1>
+      <Title />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <h1>World weather</h1>
+}
+
+export default Title
